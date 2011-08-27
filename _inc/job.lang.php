458a7ee901160eeffe2871febdbbada7ed282df9
Matthew Gall <me@matthewgall.info>
2011-08-27 22:54:20 +0100

    Started adding required phrases to language file

--- _inc/job.lang.php
+++ _inc/job.lang.php
@@ -4,4 +4,14 @@ radiPanel: Job Applications
 Language File
 */
 
+function phrase($phrase){
+    
+    static $lang = array(
+        'SUCCESS' => 'Success',
+        'ERROR' => 'Error'
+    );
+    
+    return $lang[$phrase];
+
+}
 ?>
